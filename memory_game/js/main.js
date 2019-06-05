0e1e57f3c496d22d9632c48415d8b5960c428e34
Ronald Beaney <ronaldbeaney@ronalds-mbp.mynet>
2019-06-05 09:03:49 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,28 @@
 console.log("Up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
+//var cardOne = "Queen";
+//var cardTwo = "Queen";
+//var cardThree = "King";
+//var cardFour = "King";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	} 
+}
